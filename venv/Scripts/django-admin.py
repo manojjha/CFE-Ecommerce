82079d1ffd68265b9a0accf77c6ac50b6845da74
Jha <mjha3@corp.intuit.net>
2021-01-26 00:03:04 +0530

    initial Project created

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\mjha3\Desktop\Django_project\CFE_Ecommerce\ecommerce\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
